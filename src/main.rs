380b9d31f2b5ce02d68128559a63fe1bc371b136
pott <pott@cluml.com>
2025-02-10 15:41:13 +0900

    Fix Clippy Warnings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod q1929;
 
 fn main() {
-    let result = q1929::get_concatenation(Vec::from([1, 2, 1]));
+    let result = q1929::get_concatenation(&Vec::from([1, 2, 1]));
     assert_eq!(result, vec![1, 2, 1, 1, 2, 1]);
 }
--- src/q1929.rs
+++ src/q1929.rs
@@ -1,12 +1,12 @@
-pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
+pub fn get_concatenation(nums: &Vec<i32>) -> Vec<i32> {
     let mut result: Vec<i32> = Vec::new();
 
-    for n in &nums {
+    for n in nums {
         let value: i32 = *n;
         result.push(value);
     }
 
-    for n in &nums {
+    for n in nums {
         let value: i32 = *n;
         result.push(value);
     }
