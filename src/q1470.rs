16da5f36decce5e33b481041be2aa637d1d2a67c
pott <pott@cluml.com>
2025-02-10 17:47:56 +0900

    Fix Clippy Warnings

--- src/q1470.rs
+++ src/q1470.rs
@@ -1,11 +1,10 @@
-pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+pub fn shuffle(nums: &[i32], n: usize) -> Vec<i32> {
     let mut result = Vec::with_capacity(nums.len());
 
-    let half_size: usize = n as usize;
-    for i in 0..half_size {
+    for i in 0..n {
         result.push(nums[i]);
-        result.push(nums[i + half_size]);
+        result.push(nums[i + n]);
     }
-    
+
     result
 }
