485927fa866abf5d4d9f8f52c07c55118822bee8
pott <pott@cluml.com>
2025-02-10 16:51:56 +0900

    Fix Clippy Warnings

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ mod q1920;
 mod q1929;
 
 fn main() {
-    let result = q1929::get_concatenation(&Vec::from([1, 2, 1]));
+    let result = q1929::get_concatenation(&[1, 2, 1]);
     assert_eq!(result, vec![1, 2, 1, 1, 2, 1]);
 
-    let result = q1920::build_array(Vec::from([0, 2, 1, 5, 3, 4]));
+    let result = q1920::build_array(&[0, 2, 1, 5, 3, 4]);
     assert_eq!(result, [0, 1, 2, 4, 5, 3]);
 }
--- src/q1920.rs
+++ src/q1920.rs
@@ -1,8 +1,8 @@
-pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
+pub fn build_array(nums: &[usize]) -> Vec<i32> {
     let mut result: Vec<i32> = Vec::new();
     for i in 0..nums.len() {
-        let index = nums[i] as usize;
-        result.push(nums[index]);
+        let index = nums[i];
+        result.push(nums[index].try_into().unwrap());
     }
     result
 }
