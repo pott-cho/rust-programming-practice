eee54794fc080532c1c4c6bb8f951df51326e6e2
pott <pott@cluml.com>
2025-02-17 14:17:48 +0900

    Use flat_map()

--- src/q1470.rs
+++ src/q1470.rs
@@ -1,10 +1,3 @@
 pub fn shuffle(nums: &[i32], n: usize) -> Vec<i32> {
-    let mut result = Vec::with_capacity(nums.len());
-
-    for i in 0..n {
-        result.push(nums[i]);
-        result.push(nums[i + n]);
-    }
-
-    result
+    (0..n).flat_map(|i| vec![nums[i], nums[i + n]]).collect()
 }
