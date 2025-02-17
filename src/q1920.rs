a89b451daa7f56de162d1fea60953312b42b0751
pott <pott@cluml.com>
2025-02-17 10:03:53 +0900

    Use filter_map()

--- src/q1920.rs
+++ src/q1920.rs
@@ -1,8 +1,3 @@
-pub fn build_array(nums: &[usize]) -> Vec<i32> {
-    let mut result: Vec<i32> = Vec::new();
-    for i in 0..nums.len() {
-        let index = nums[i];
-        result.push(nums[index].try_into().unwrap());
-    }
-    result
+pub fn build_array(nums: &[usize]) -> Vec<usize> {
+    nums.iter().filter_map(|&i| nums.get(i)).copied().collect()
 }
