5847e3ac4accad10d5053277d1f00d706893d2b4
pott <pott@cluml.com>
2025-02-18 12:53:04 +0900

    Fix Code Style

--- src/q1365.rs
+++ src/q1365.rs
@@ -3,6 +3,6 @@ pub fn smaller_numbers_than_current(nums: &[usize]) -> Vec<usize> {
         .iter()
         .map(|x| nums.iter().filter(|y| **y < *x).count())
         .collect();
-    
+
     result
 }
