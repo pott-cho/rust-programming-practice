98d9d15afa5cff489d940d5a0b6f7a6f317833a1
pott <pott@cluml.com>
2025-02-18 14:11:37 +0900

    Fix unwrap()

--- src/q771.rs
+++ src/q771.rs
@@ -1,5 +1,6 @@
 pub fn num_jewels_in_stones(jewels: &str, stones: &str) -> i32 {
     let result = stones.chars().filter(|x| jewels.contains(*x)).count();
 
-    result.try_into().unwrap()
+    // stones 의 길이가 매우 커서 result가 i32 범위를 넘어가면 -1을 반환한다.
+    result.try_into().unwrap_or(-1)
 }
