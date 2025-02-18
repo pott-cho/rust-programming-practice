3b371f0613b58fd81c87051928e938fe44e12292
pott <pott@cluml.com>
2025-02-18 13:10:26 +0900

    Use filter(), count()

--- src/q771.rs
+++ src/q771.rs
@@ -1,10 +1,5 @@
 pub fn num_jewels_in_stones(jewels: &str, stones: &str) -> i32 {
-    let mut result = 0;
-    for c in stones.chars() {
-        if jewels.contains(c) {
-            result += 1;
-        }
-    }
+    let result = stones.chars().filter(|x| jewels.contains(*x)).count();
 
-    result
+    result.try_into().unwrap()
 }
