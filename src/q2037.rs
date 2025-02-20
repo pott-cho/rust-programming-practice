e32275ae40898563d31290f85014fb3dcb606b96
pott <pott@cluml.com>
2025-02-20 16:55:27 +0900

    Use mut, map()

--- src/q2037.rs
+++ src/q2037.rs
@@ -1,14 +1,8 @@
-pub fn min_moves_to_seat(seats: &[i32], students: &[i32]) -> i32 {
-    let mut seats = seats.to_vec();
-    let mut students = students.to_vec();
-
+pub fn min_moves_to_seat(seats: &mut [i32], students: &mut [i32]) -> i32 {
     seats.sort_unstable();
     students.sort_unstable();
 
-    let mut total_moves = 0;
-    for i in 0..seats.len() {
-        total_moves += (seats[i] - students[i]).abs();
-    }
-
-    total_moves
+    (0..seats.len())
+        .map(|x| (seats[x] - students[x]).abs())
+        .sum()
 }
