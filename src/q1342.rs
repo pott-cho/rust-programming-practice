aeadf655c4e5dc6ad189c21b5dbddaaa178067e8
pott <pott@cluml.com>
2025-02-20 16:23:49 +0900

    Use leading_zeros(), count_ones()

--- src/q1342.rs
+++ src/q1342.rs
@@ -1,13 +1,10 @@
-pub fn number_of_steps(num: i32) -> i32 {
-    let mut number = num;
-    let mut cnt = 0;
-    while number > 0 {
-        if number % 2 == 0 {
-            number /= 2;
-        } else {
-            number -= 1;
-        }
-        cnt += 1;
+pub fn number_of_steps(num: i32) -> u32 {
+    if num == 0 {
+        return 0;
     }
-    cnt
+
+    let even = 32 - num.leading_zeros(); // 이진수의 크기 == 나누기 2를 해야하는 횟수
+    let odd = num.count_ones(); // 1의 개수 == 빼기 1을 해야하는 횟수
+
+    even + odd - 1 // 마지막 1처리
 }
