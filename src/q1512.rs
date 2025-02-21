9aa758b5a788446fee0f95481337c340c2fc0254
pott <pott@cluml.com>
2025-02-21 12:50:17 +0900

    Fix Rotate Style

--- src/q1512.rs
+++ src/q1512.rs
@@ -5,8 +5,8 @@ pub fn num_identical_pairs(nums: &[i32]) -> i32 {
     let mut map: HashMap<i32, i32> = HashMap::new();
 
     // 현재 값을 해시맵에서 찾는다. 있으면 +1, 없으면 entry 1 등록
-    for n in nums {
-        map.entry(*n)
+    for &n in nums {
+        map.entry(n)
             .and_modify(|e| {
                 *e += 1;
                 result += *e;
