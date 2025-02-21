f361b041c6a636f0451bbdb750b0f2ef7f824ead
pott <pott@cluml.com>
2025-02-21 13:19:56 +0900

    Convert match to map

--- src/q1431.rs
+++ src/q1431.rs
@@ -1,9 +1,6 @@
 pub fn kids_with_candies(candies: &[i32], extra_candies: i32) -> Vec<bool> {
     // 1. 가장 큰 캔디수를 구한다.
-    let max = match candies.iter().max() {
-        Some(&max_value) => max_value,
-        None => 0,
-    };
+    let max = candies.iter().max().map_or(0, |&x| x);
 
     // 2. (현재 캔디수 + 여분 캔디수) >= 가장 큰 캔디수 이면 true, 그렇지 않으면 false.
     candies.iter().map(|x| x + extra_candies >= max).collect()
