b0aaafa657044d75d9da217272aa7a47d1cf1168
pott <pott@cluml.com>
2025-02-21 13:27:47 +0900

    Change to direct return

--- src/q1365.rs
+++ src/q1365.rs
@@ -1,8 +1,5 @@
 pub fn smaller_numbers_than_current(nums: &[usize]) -> Vec<usize> {
-    let result = nums
-        .iter()
+    nums.iter()
         .map(|x| nums.iter().filter(|y| **y < *x).count())
-        .collect();
-
-    result
+        .collect()
 }
