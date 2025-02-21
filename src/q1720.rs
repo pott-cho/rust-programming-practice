1c0645d2481e580d358c5adc3732d4bb03d7919d
pott <pott@cluml.com>
2025-02-21 15:24:25 +0900

    Erase duplicate copy using once() and chain()

--- src/q1720.rs
+++ src/q1720.rs
@@ -1,14 +1,10 @@
+use std::iter::once;
+
 pub fn decode(encoded: &[i32], first: i32) -> Vec<i32> {
-    let decoded: Vec<i32> = encoded
-        .iter()
-        .scan(first, |state, &e| {
+    once(first)
+        .chain(encoded.iter().scan(first, |state, &e| {
             *state ^= e;
             Some(*state)
-        })
-        .collect();
-
-    let mut result = vec![first];
-    result.extend(decoded);
-
-    result
+        }))
+        .collect()
 }
