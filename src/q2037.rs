5c59d804e70fd3bf544047966b16204465681126
pott <pott@cluml.com>
2025-02-24 16:37:47 +0900

    Change [] to zip()

--- src/q2037.rs
+++ src/q2037.rs
@@ -2,7 +2,9 @@ pub fn min_moves_to_seat(seats: &mut [i32], students: &mut [i32]) -> i32 {
     seats.sort_unstable();
     students.sort_unstable();
 
-    (0..seats.len())
-        .map(|x| (seats[x] - students[x]).abs())
+    seats
+        .iter()
+        .zip(students.iter())
+        .map(|(s1, s2)| (s1 - s2).abs())
         .sum()
 }
