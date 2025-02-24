74fd06451cfb898bb56bfb8d74de8fbc08ccc8b3
pott <pott@cluml.com>
2025-02-24 14:46:40 +0900

    Use sort_unstable_by()

--- src/q1528.rs
+++ src/q1528.rs
@@ -1,6 +1,5 @@
 pub fn restore_string(s: &str, indices: &[usize]) -> String {
-    let ch: Vec<char> = s.chars().collect();
-    let mut combi: Vec<(char, &usize)> = ch.into_iter().zip(indices).collect();
-    combi.sort_by_key(|&(_, index)| index);
+    let mut combi: Vec<(char, &usize)> = s.chars().zip(indices.iter()).collect();
+    combi.sort_unstable_by(|a, b| a.1.cmp(b.1));
     combi.iter().map(|(x, _)| x).collect()
 }
