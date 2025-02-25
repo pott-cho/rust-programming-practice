d86658f5ce22d8b21b82c5d6e07103080551eeb5
pott <pott@cluml.com>
2025-02-25 10:00:08 +0900

    Use extend()

--- src/q1720.rs
+++ src/q1720.rs
@@ -1,10 +1,9 @@
-use std::iter::once;
-
 pub fn decode(encoded: &[i32], first: i32) -> Vec<i32> {
-    once(first)
-        .chain(encoded.iter().scan(first, |state, &e| {
-            *state ^= e;
-            Some(*state)
-        }))
-        .collect()
+    let mut result = vec![first];
+    result.extend(encoded.iter().scan(first, |state, &e| {
+        *state ^= e;
+        Some(*state)
+    }));
+
+    result
 }
